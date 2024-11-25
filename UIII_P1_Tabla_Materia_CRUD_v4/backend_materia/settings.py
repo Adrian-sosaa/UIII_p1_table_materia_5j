eb8da5cac50453f3586f8bcfef4d4f466edacfa0
Adrian-sosaa <a.22308051281317@cbtis128.edu.mx>
2024-11-25 11:04:41 -0600

    v5

--- UIII_P1_Tabla_Materia_CRUD_v4/backend_materia/settings.py
+++ UIII_P1_Tabla_Materia_CRUD_v4/backend_materia/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os #trabajar con directorios
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'materia_app',
+    'app_base'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'backend_materia.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +120,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_URL=  '/static/'
+STATICFILES_DIRS =(os.path.join(BASE_DIR,"static"),)
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
